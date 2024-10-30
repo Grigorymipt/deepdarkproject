c502c87085adf8edd7cd37e4fbb992c2b5975704
Grigorymipt <koveshnikov.gv@phystech.edu>
2024-10-30 17:23:57 +0300

    fix llama bugs

--- AskLlama/Program.cs
+++ AskLlama/Program.cs
@@ -25,19 +25,16 @@ Func<string, Task<string>> askLlama = async input =>
 {
     string url = "http://localhost:11434/api/generate";
     string prompt = input;
-    string filename = args[0];
-
-    // Read the content of the file
-    string text = File.ReadAllText(filename);
 
     // Prepare the request headers and data
-    var client = new HttpClient();
-    client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+    var client = new HttpClient(){
+        Timeout = TimeSpan.FromMinutes(3) // Increase timeout to 5 minutes
+    };
 
     var data = new
     {
-        model = "llama3.1",
-        prompt = prompt + text,
+        model = "bambucha/saiga-llama3:8b-q4_K",
+        prompt = prompt,
         stream = false
     };
 
@@ -52,13 +49,15 @@ Func<string, Task<string>> askLlama = async input =>
     {
         string responseText = await response.Content.ReadAsStringAsync();
         var jsonResponse = JObject.Parse(responseText);
-        string actualResponse = jsonResponse["response"]?.ToString();
+        string actualResponse = jsonResponse["response"]?.ToString() 
+                                ?? throw new InvalidOperationException("Llama responded with null");
         return actualResponse;
     }
     else
     {
         throw new Exception($"Error: {response.StatusCode}, {response.Content.ReadAsStringAsync().Result}");
     }
+    
 };
 
 var texts = files.Select(fileInfo =>
@@ -81,11 +80,11 @@ for (int i = 0; i < texts.Count; i++)
 {
     for (int j = i + 1; j < texts.Count; j++)
     {
-        ties.Add($"{prompt} {texts[i]} {texts[j]}");
+        ties.Add($"{prompt} \n Text 1:\n {texts[i]} \n Text 2:\n {texts[j]}");
     }
 }
 
-var outputs = ties.Select(t => askLlama(t));
+var outputs = ties.Select(t => askLlama(t).Result);
 foreach (var output in outputs) { Console.WriteLine(output); }
 
 Console.WriteLine("Finished");
