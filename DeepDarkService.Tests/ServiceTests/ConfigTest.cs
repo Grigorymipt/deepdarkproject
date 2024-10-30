754f177c4bc91f35fe1bdeba56c973c5b1a76ded
Grigorymipt <koveshnikov.gv@phystech.edu>
2024-10-30 12:29:00 +0300

    changes

--- DeepDarkService.Tests/ServiceTests/ConfigTest.cs
+++ DeepDarkService.Tests/ServiceTests/ConfigTest.cs
@@ -1,6 +1,18 @@
+using Microsoft.Extensions.Configuration;
+using Xunit.Abstractions;
+
 namespace DeepDarkService.Tests.ServiceTests;
 
 public class ConfigTest
-{
+{ 
+    private readonly ITestOutputHelper _testOutputHelper;
+
     
+
+    [Fact]
+    public void Test_Config()
+    {
+        // _testOutputHelper.WriteLine("Your variable: "+_configuration["Environment"].FirstOrDefault().ToString());
+        Assert.True(1 == 1);
+    }
 }
--- DeepDarkService/Program.cs
+++ DeepDarkService/Program.cs
@@ -2,6 +2,7 @@ using DeepDarkService.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCustomServices(builder.Configuration);
+builder.Configuration.AddJsonFile("appsettings.json", optional: false);
 
 var app = builder.Build();
 app.UseCustomMiddleware();
